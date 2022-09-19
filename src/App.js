85bffd2b50dbf43f46465844471798b3701c3ded
Subrat Pandey <subratpandey974@gmail.com>
2022-09-19 09:12:43 -0500

    Created calculator app using React.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const [calc, setCalc] = useState("");
+	const [result, setResult] = useState("");
+
+	const operators = ['*', '/', '+', '-', '.'];
+
+	const updateCalc = (value) => {
+		if (
+			operators.includes(value) && calc === "" ||
+			operators.includes(value) && operators.includes(calc.slice(-1)
+			)
+		) {
+			return;
+		}
+
+		setCalc(calc + value);
+
+		if (!operators.includes(value)) {
+			setResult(eval(calc + value).toString());
+		}
+	}
+
+	const createDigits = () => {
+		const digits = [];
+
+		for (let i = 1; i < 10; i++){
+			digits.push(
+				<button 
+					onClick={() => updateCalc(i.toString())} 
+					key = {i}> 
+						{i} 
+				</button>
+			)
+		}
+		return digits;
+	}
+
+	const calculate = () => {
+		setCalc(eval(calc).toString());
+	}
+
+	const deleteLast = () => {
+		if (calc == ""){
+			return;
+		}
+		const value = calc.slice(0, -1);
+		setCalc(value);
+	}
+
+	return (
+		<div className="App">
+			<div className="fi-calculator">
+
+				<div className="fi-display">
+					{ result ? <span>({result})</span>: "" } 
+					&nbsp;
+					{ calc || "0"}
+				</div>
+
+				<div className="fi-operators" >
+					<button onClick={() => updateCalc('*')}>*</button>
+					<button onClick={() => updateCalc('/')}>/</button>
+					<button onClick={() => updateCalc('+')}>+</button>
+					<button onClick={() => updateCalc('-')}>-</button>
+
+					<button onClick={deleteLast}>DEL</button>
+				</div>
+
+				<div className="fi-digits" >
+					{ createDigits() }
+					<button onClick={() => updateCalc('0')}>0</button>
+					<button onClick={() => updateCalc('.')}>.</button>
+
+					<button onClick={calculate}>=</button>				
+				</div>
+
+			</div>
+		</div>
+	);
 }
 
 export default App;
